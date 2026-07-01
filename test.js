175abdaa7f1c8f517068257d64d1ca1fbe840e53
Fahim Muntasir <fmuntasir488@gmail.com>
2026-07-01 18:14:54 +0600

    update

---
+++ test.js
@@ -0,0 +1,8 @@
+function fahim2(name)
+{
+    document.getElementById("fahim2").innerHTML=name+" is a good boy ";
+}
+function displayDate()
+{
+    document.getElementById("time").innerHTML=Date();
+}
